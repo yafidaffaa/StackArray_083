1351464ae02e67780a6e871f953e64965e16a16d
yafidaffaa <yafi.d.andriansyah@gmail.com>
2023-06-16 15:07:13 +0700

    menampilkan semua data

--- stackArray/stackArray.cpp
+++ stackArray/stackArray.cpp
@@ -46,5 +46,10 @@ public:
 		if (empty()) {
 			cout << "\nStack is empty." << endl;
 		}
+		else {
+			for (int tmp = top; tmp >= 0; tmp--) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
 	}
 };
